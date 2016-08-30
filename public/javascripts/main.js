969272a5953da5d560c4bfab85653454d9916744
liamscott <liamscott@greenhouse.io>
2016-08-30 15:03:07 -0400

    Add record update and destroy functionality

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,7 +13,7 @@ $(function(){
 			url: '/create-student',
 			success: function(data) {
 				document.getElementById('new-student').reset();
-				addStudent(data[0].value, data[1].value, data[2].value, data[3].value);
+				addStudent(data[0].value, data[1].value, data[2].value, data[3].value, data[4].value);
 			}
 		});
 		event.preventDefault();
@@ -30,22 +30,39 @@ $(function(){
 				this.value = parseFloat($(this).val()).toFixed(1);
 			}
 		});
-	})
+	});
+
+	/*
+	 * Delete Student
+	 */
+	$('td.edit-link').find('a[title="Delete"]').on('click', function(e){
+		var $this = $(this);
+		e.preventDefault();
+		$.ajax({
+			type: 'GET',
+			url: $(this).attr('href'),
+			success: function(data) {
+				$this.closest('tr.student').fadeOut(300, function(){
+					$(this).remove();
+				});
+				$('.student-deleted').removeClass('hidden');
+			}
+		});
+	});
 
 	/*
 	 * Add new student to the document
 	 */
-	function addStudent(firstName, lastName, classId, gpa) {
+	function addStudent(firstName, lastName, classId, gpa, id) {
 		// Get index of the new student based on current last student index
 		var currentIndex = parseInt( $('table#current-students .student:last-child').find('.student-num').text() ) || 0;
 		var newIndex = currentIndex + 1;
 		// Create HTML string for new student
-		var $newStudent = $('<tr class="student"><th class="student-num">'+newIndex+'</th><td class="student-first-name">'+firstName+'</td><td class="student-last-name">'+lastName+'</td><td class="student-class">'+classId+'</td><td class="student gpa">'+gpa+'</td></tr>').hide();
+		var $newStudent = $('<tr class="student"><th class="student-num">'+newIndex+'</th><td class="student-first-name">'+firstName+'</td><td class="student-last-name">'+lastName+'</td><td class="student-class">'+classId+'</td><td class="student gpa">'+gpa+'</td><td class="edit-link"><a title="Edit" href="/edit/'+id+'"><i class="glyphicon glyphicon-pencil"></i></a><a href="/destroy/'+id+'" title="Delete" class="text-danger" style="padding-left:20px"><i class="glyphicon glyphicon-trash"></i></a></td></tr>').hide();
 		// Add new student to document
 		$('table#current-students').append($newStudent);
-		$newStudent.fadeIn('slow', function(){
-			// Trigger success banner
-			$('.alert-success').removeClass('hidden');
-		});
+		$newStudent.fadeIn('slow');
+		// Trigger success banner
+		$('.student-created').removeClass('hidden');
 	}
 });
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,7 @@ var router = express.Router();
 
 router.get('/', function (req, res) {
     Student.find({}, function(err, students){
-        if (err) {
-            throw err;
-            console.log(err);
-        }
-        console.log("fetched students");
+        if (err) throw err;
         res.render('index', {
             user : req.user,
             students: students
@@ -55,11 +51,49 @@ router.post('/create-student', function(req, res) {
         class_id: req.body[2].value,
         gpa: req.body[3].value
     });
-    newStudent.save(function(err){
+    newStudent.save(function(err, student){
         if (err) throw err;
 
         console.log('Student Created');
+        req.body.push({name: 'id', value: student.id});
+        console.log(req.body);
         res.send(req.body);
+    });
+});
+
+router.get('/edit/:id', function(req, res) {
+    var id = req.params.id;
+    Student.findOne({'_id': id}, function(err, student){
+        if (err) throw err;
+
+        res.render('edit', {
+            student: student,
+            user : req.user
+        });
+    });
+});
+
+router.post('/edit', function(req, res) {
+    var studentId = req.body.student_id,
+        firstName = req.body.first_name,
+        lastName = req.body.last_name,
+        classNum = req.body.class,
+        gpa = req.body.gpa;
+
+        console.log(req.body);
+
+    Student.findOneAndUpdate({'_id': studentId}, {first_name: firstName, last_name: lastName, class_id: classNum, gpa: gpa}, function(err, student) {
+        if (err) throw err;
+        res.redirect('/');
+    });
+});
+
+router.get('/destroy/:id', function(req, res) {
+    var id = req.params.id;
+    Student.findOneAndRemove({'_id': id}, function(err, student){
+        if (err) throw err;
+        console.log('student deleted');
+        res.send(student);
     });
 });
 
