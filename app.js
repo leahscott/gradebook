5ed40832625eb1746300f5f8795c561b438e79e4
liamscott <liamscott@greenhouse.io>
2016-09-21 11:11:44 -0400

    Save session information in Redis
    This allows sessions to persist across server restarts. Sessions were
    previously being stored in process memory.

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var RedisStore = require('connect-redis')(session);
 
 var routes = require('./routes/index');
 
@@ -23,7 +25,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('express-session')({
+app.use(session({
+    store: new RedisStore({
+        host: 'localhost',
+        port: 6379
+    }),
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false
